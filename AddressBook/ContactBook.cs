eb691e3c093028635fe7aa17e46d0df741b100c4
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-03 22:20:16 +0530

    [Harshitha] Adding Contact details

---
+++ AddressBook/ContactBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class ContactBook
+    {
+        public static int cnt = 0;
+        //Creating list to add contact details
+        public List<ContactDetails> contactList;
+        public ContactBook()
+        {
+            this.contactList = new List<ContactDetails>();
+        }
+        //Adding the contact details to address book
+        public void AddContact(ContactDetails cd)
+        {
+            
+            this.contactList.Add(cd);
+            cnt++;
+        }
+        //Finding phone number
+        public int FindByPhonenumber(long number)
+        {
+            return contactList.FindIndex(cd=>cd.phonenumber==number);
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,7 +7,41 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
-            Contacts.ReadInput();
+            ContactBook book = new ContactBook();
+            
+            int option = 1;
+            //Choosing option
+            while (option == 1)
+            {
+                Console.WriteLine("Choose an option to perform : ");
+                Console.WriteLine("1.Adding the contact details to Address Book");
+                Console.WriteLine("2.Exit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        //Adding contact details
+                        ContactDetails cd = new ContactDetails();
+                        cd.ReadInput();
+                        int index = book.FindByPhonenumber(cd.phonenumber);
+                        if (index == -1)
+                        {
+                            book.AddContact(cd);
+                            Console.WriteLine("Contact updated successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Contact already exists");
+                        }
+                        break;
+                    case 2:
+                        //Exit
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("Count of contacts in address book : "+ContactBook.cnt);
         }
     }
 }
